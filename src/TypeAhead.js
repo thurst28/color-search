ff9f64887a0d1427cabf83669fc4d26fb3b70c02
tim <thurst@stetson.edu>
2020-07-05 12:24:40 -0400

    Refactor to use reducer

--- src/TypeAhead.js
+++ src/TypeAhead.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef, useReducer } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Lottie from "react-lottie";
@@ -17,40 +17,76 @@ const lottieAnimationOptions = {
   },
 };
 
+const Action = {
+  INPUT: "INPUT",
+  WORD_SUBMIT: "WORD_SUBMIT",
+  CLOSE_LIST: "CLOSE_LIST",
+  OPEN_LIST: "OPEN_LIST",
+};
+
 const Status = {
   ACTIVE: "ACTIVE",
   INACTIVE: "INACTIVE",
-  SET_WORD: "SET_WORD",
+};
+
+const initialState = {
+  status: Status.INACTIVE,
+  searchTerm: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case Action.INPUT: {
+      const { searchTerm } = action.payload;
+      const activeSearchTerm = searchTerm.trim();
+      debugger;
+      return {
+        ...state,
+        status: activeSearchTerm ? Status.ACTIVE : Status.INACTIVE,
+        searchTerm,
+      };
+    }
+    case Action.WORD_SUBMIT: {
+      const { word } = action.payload;
+      return {
+        ...state,
+        status: Status.INACTIVE,
+        searchTerm: word,
+      };
+    }
+    case Action.CLOSE_LIST: {
+      return {
+        ...state,
+        status: Status.INACTIVE,
+      };
+    }
+    case Action.OPEN_LIST: {
+      const activeSearchTerm = state.searchTerm.trim();
+      return {
+        ...state,
+        status: activeSearchTerm ? Status.ACTIVE : Status.INACTIVE,
+      };
+    }
+    default:
+      return state;
+  }
 };
 
 function TypeAhead(props) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [status, setStatus] = useState(Status.INACTIVE);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const containerRef = useRef(null);
-
-  // SearchTerm
-  useEffect(() => {
-    // if a searchTerm is not an "" after a change, we should make the page active
-    const activeSearchTerm = searchTerm.trim();
-    if (activeSearchTerm && status !== Status.SET_WORD) {
-      setStatus(Status.ACTIVE);
-    } else {
-      setStatus(Status.INACTIVE);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchTerm]);
 
   // Set up keyboard and click listeners
   useEffect(() => {
     const handleClick = (event) => {
-      if (containerRef.current && !containerRef.current.contains(event.target) && status !== Status.SET_WORD) {
-        setStatus(Status.INACTIVE);
+      if (containerRef.current && !containerRef.current.contains(event.target)) {
+        dispatch({ type: Action.CLOSE_LIST });
       }
     };
 
     const handleKeyDown = (event) => {
       if (event.key === "Escape") {
-        setStatus(Status.INACTIVE);
+        dispatch({ type: Action.CLOSE_LIST });
         document.activeElement.blur();
       }
     };
@@ -71,7 +107,7 @@ function TypeAhead(props) {
 
   const handleSearchTermChange = (event) => {
     const { value } = event.target;
-    setSearchTerm(value);
+    dispatch({ type: Action.INPUT, payload: { searchTerm: value } });
   };
 
   const handleBlur = () => {
@@ -81,23 +117,18 @@ function TypeAhead(props) {
     let childToTabTo = childIndex !== undefined && childIndex !== -1;
 
     if (!childToTabTo) {
-      setStatus(Status.INACTIVE);
+      dispatch({ type: Action.CLOSE_LIST });
     }
   };
 
   const handleWordClick = (word) => {
-    setActiveWord(word);
+    dispatch({ type: Action.WORD_SUBMIT, payload: { word } });
   };
 
   const handleWordKeyDown = (event, word) => {
     if (event.key === "Enter") {
-      setActiveWord(word);
+      dispatch({ type: Action.WORD_SUBMIT, payload: { word } });
     }
-  };
-
-  const setActiveWord = (word) => {
-    setSearchTerm(word);
-    setStatus(Status.SET_WORD);
   };
 
   /*
@@ -107,7 +138,7 @@ function TypeAhead(props) {
   const getDisplayList = () => {
     const matchedWords = [];
     //remove whitespace
-    let matchingStr = searchTerm.replace(/\s/g, "");
+    let matchingStr = state.searchTerm.replace(/\s/g, "");
     //escape any regex charcters a user might input
     matchingStr = matchingStr.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
     // regex to find search term and begining and keep delimiter
@@ -143,7 +174,7 @@ function TypeAhead(props) {
         height={"100px"}
         width={"150px"}
         options={lottieAnimationOptions}
-        isPaused={status === Status.INACTIVE}
+        isPaused={state.status === Status.INACTIVE}
       />
       <Container data-testid="main-container" ref={containerRef}>
         <InputContainer id="inputContainer">
@@ -151,13 +182,13 @@ function TypeAhead(props) {
           <input
             type="text"
             id="searchTerm"
-            value={searchTerm}
+            value={state.searchTerm}
             onChange={handleSearchTermChange}
-            onFocus={() => searchTerm.trim() && setStatus(Status.ACTIVE)}
+            onFocus={() => dispatch({ type: Action.OPEN_LIST })}
             onBlur={handleBlur}
           />
         </InputContainer>
-        {status === Status.ACTIVE && (
+        {state.status === Status.ACTIVE && (
           <List words={getDisplayList()} handleClick={handleWordClick} handleKeyDown={handleWordKeyDown} />
         )}
       </Container>
